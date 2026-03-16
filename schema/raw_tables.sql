f734509ba86414b7dd72fecd6ea5d991af467a2b
Mark Bachurin <mark.bachurin@jetbrains.com>
2026-03-16 15:51:55 +0100

    Fix typo

--- schema/raw_tables.sql
+++ schema/raw_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS raw_candidates (
     candidate_id SERIAL PRIMARY KEY,
     full_name TEXT NOT NULL,
     source TEXT NOT NULL,
-    profile_create_date DATE NOT NULL
+    profile_created_date DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS raw_applications (
