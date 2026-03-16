7b08659a8c24edac26b5f428bc9ebedffc9fb5e5
Mark Bachurin <mark.bachurin@jetbrains.com>
2026-03-16 15:36:43 +0100

    Add raw_applications, raw_interviews tables

--- schema/raw_tables.sql
+++ schema/raw_tables.sql
@@ -5,3 +5,18 @@ CREATE TABLE IF NOT EXISTS raw_candidates (
     profile_create_date DATE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS raw_applications (
+    app_id SERIAL PRIMARY KEY,
+    candidate_id INTEGER REFERENCES raw_candidates(candidate_id),
+    role_level TEXT NOT NULL CHECK (role_level IN ('Junior', 'Senior', 'Executive')),
+    applied_date DATE NOT NULL,
+    decision_date DATE,
+    expected_salary NUMERIC
+);
+
+CREATE TABLE IF NOT EXISTS raw_interviews(
+    interview_id SERIAL PRIMARY KEY,
+    app_id INTEGER REFERENCES raw_applications(app_id),
+    interview_date DATE NOT NULL,
+    outcome TEXT NOT NULL CHECK (outcome IN ('Passed', 'Rejected', 'No Show'))
+);
